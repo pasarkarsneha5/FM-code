de3966386fa9cdcbf83ec0ae3f7d5e020783037b
pasarkarsneha5 <you@example.comsneha.pasarkar@saviantconsulting.com>
2023-02-08 12:35:12 +0530

    changes in POM

--- src/main/java/com/staging/fm/utils/ElementUtil.java
+++ src/main/java/com/staging/fm/utils/ElementUtil.java
@@ -19,7 +19,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.xml.sax.Locator;
+//import org.xml.sax.Locator;
 
 import com.staging.fm.factory.DriverFactory;
 
